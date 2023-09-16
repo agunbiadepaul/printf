32179a6bf2cc476c233afe8289a51dd6bbf433cf
“christiancs50” <christian.gaoso@gmail.com>
2023-09-16 15:08:01 -0700

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to a format.
+ *
+ * @format: 
+ *
+ * Return: the number of characters except NULL
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
