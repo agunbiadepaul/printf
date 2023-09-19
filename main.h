264a22e0a652d0ba316fadbc51bb705b41b75706
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-09-18 21:31:08 -0700

    update

--- main.h
+++ main.h
@@ -4,9 +4,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
 void print_string(va_list pcargs, int *numb_of_chars_printed);
 int _printf(const char *format, ...);
 void print_int(va_list args, int *printed_chars);
+void print_binary(unsigned int n, int *numb_of_chars_printed);
 
 #endif /* _MAIN_H_ */
